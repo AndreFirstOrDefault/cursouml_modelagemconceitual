d8b7fbfa5721035562ef309e1e04efb894da82e0
Andre Luiz <andre.java.ads.2022@gmal.com>
2023-05-05 14:54:39 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/multifocal/cursomc/resources/ClienteResource.java
+++ src/main/java/com/multifocal/cursomc/resources/ClienteResource.java
@@ -1,25 +1,28 @@
 package com.multifocal.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.multifocal.cursomc.domain.Categoria;
-import com.multifocal.cursomc.services.CategoriaService;
+import com.multifocal.cursomc.domain.Cliente;
+import com.multifocal.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResource {
+@RequestMapping(value="/clientes")
+public class ClienteResource {
 	
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
-	@GetMapping("/{id}")
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 		
 	}
