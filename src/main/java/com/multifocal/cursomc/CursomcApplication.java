99c75cbdf051ab51fc497c0525dcb0f7d2d4706e
Andre Luiz <andre.java.ads.2022@gmal.com>
2023-05-08 19:46:13 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/multifocal/cursomc/CursomcApplication.java
+++ src/main/java/com/multifocal/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.multifocal.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.multifocal.cursomc.domain.Cidade;
 import com.multifocal.cursomc.domain.Cliente;
 import com.multifocal.cursomc.domain.Endereco;
 import com.multifocal.cursomc.domain.Estado;
+import com.multifocal.cursomc.domain.Pagamento;
+import com.multifocal.cursomc.domain.PagamentoComBoleto;
+import com.multifocal.cursomc.domain.PagamentoComCartao;
+import com.multifocal.cursomc.domain.Pedido;
 import com.multifocal.cursomc.domain.Produto;
+import com.multifocal.cursomc.domain.enums.EstadoPagamento;
 import com.multifocal.cursomc.domain.enums.TipoCliente;
 import com.multifocal.cursomc.repositories.CategoriaRepository;
 import com.multifocal.cursomc.repositories.CidadeRepository;
 import com.multifocal.cursomc.repositories.ClienteRepository;
 import com.multifocal.cursomc.repositories.EnderecoRepository;
 import com.multifocal.cursomc.repositories.EstadoRepository;
+import com.multifocal.cursomc.repositories.PagamentoRepository;
+import com.multifocal.cursomc.repositories.PedidoRepository;
 import com.multifocal.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -84,6 +96,22 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null,sdf.parse("30/09/2022 10:32"),cli1,e1);
+		Pedido ped2 = new Pedido(null,sdf.parse("10/10/2022 19:35"),cli1,e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2022 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 	}
 
 }
